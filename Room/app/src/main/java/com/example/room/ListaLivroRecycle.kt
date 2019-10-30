24ec76c55edbd63db98561265ff53950792dabb8
Daniel Soares <legoliga123@gmail.com>
2019-10-30 10:43:58 -0300

    problemas ao clicar

--- Room/app/src/main/java/com/example/room/ListaLivroRecycle.kt
+++ Room/app/src/main/java/com/example/room/ListaLivroRecycle.kt
@@ -2,10 +2,15 @@ package com.example.room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.room.Adapter.LivrosAdapterRecycle
 import com.example.room.Entity.Livro
+import com.example.room.RecycleClickLisneter.MyRecycleViewClickListener
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_lista_livro_recycle.*
 
 class ListaLivroRecycle : AppCompatActivity() {
@@ -18,9 +23,40 @@ class ListaLivroRecycle : AppCompatActivity() {
         recyclerview.adapter = adapter
 
 
-        //val layout = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        val layout = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        //val layout = GridLayoutManager(this,4)
+        //val layout = StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.VERTICAL)
 
-        val layout = GridLayoutManager(this,4)
+        recyclerview.addOnItemTouchListener(
+            MyRecycleViewClickListener(
+                this@ListaLivroRecycle,
+                recyclerview,
+                object : MyRecycleViewClickListener.OnItemClickListener{
+                    override fun onItemClick(view: View, position: Int) {
+                        Toast.makeText(this@ListaLivroRecycle,"Click simples interface",Toast.LENGTH_SHORT)
+                            .show()
+                    }
+
+                    override fun onItemLongClick(view: View, position: Int) {
+                        val removida = MainActivity.livros[position]
+                        MainActivity.livros.remove(removida)
+                        recyclerview.adapter!!.notifyItemRemoved(position)
+                        Toast.makeText(this@ListaLivroRecycle,"Clique longo interface", Toast.LENGTH_SHORT)
+                            .show()
+                        val snack = Snackbar.make(
+                            recyclerview.parent as View,
+                            "Removido com sucesso!!",
+                            Snackbar.LENGTH_LONG)
+                            .setAction("Cancelar?"){
+                                MainActivity.livros.add(position,removida)
+                                recyclerview.adapter!!.notifyItemInserted(position)
+                            }
+                        snack.show()
+
+                    }
+                }
+            )
+        )
 
         recyclerview.layoutManager = layout
     }
--- Room/app/src/main/java/com/example/room/RecycleClickLisneter/MyRecycleViewClickListener.kt
+++ Room/app/src/main/java/com/example/room/RecycleClickLisneter/MyRecycleViewClickListener.kt
@@ -1,21 +1,52 @@
 package com.example.room.RecycleClickLisneter
 
+import android.content.Context
+import android.util.Log
+import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
 import android.widget.AdapterView
 import androidx.recyclerview.widget.RecyclerView
 import java.text.FieldPosition
 
-class MyRecycleViewClickListener : RecyclerView.OnItemTouchListener{
+class MyRecycleViewClickListener(val context: Context, val view:RecyclerView,val listener:OnItemClickListener) : RecyclerView.OnItemTouchListener{
 
-    var listener : OnItemClickListener? = null
+    var myGestureDetector : GestureDetector
+
 
     interface OnItemClickListener{
         fun onItemClick(view: View, position: Int)
         fun onItemLongClick(view: View, position: Int)
     }
 
+    init {
+        myGestureDetector = GestureDetector(context,object :GestureDetector.SimpleOnGestureListener(){
+            override fun onSingleTapUp(motionEvent: MotionEvent?): Boolean {
+                return super.onSingleTapUp(motionEvent)
+                val childView = view.findChildViewUnder(motionEvent!!.x, motionEvent.y)
+                if(childView != null){
+                    listener.onItemClick(childView,view.getChildAdapterPosition(childView))
+                    Log.i("Teste","onSingleTapUp")
+                }
+                return true
+            }
+
+            override fun onLongPress(motionEvent: MotionEvent?) {
+                super.onLongPress(motionEvent)
+                val childView = view.findChildViewUnder(motionEvent!!.x,motionEvent.y)
+                if (childView != null){
+                    listener.onItemLongClick(
+                        childView,
+                        view.getChildAdapterPosition(childView)
+                    )
+                    Log.i("Teste", "onLongPress")
+                }
+            }
+        })
+    }
+
     override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+        myGestureDetector.onTouchEvent(e)
         return false
     }
 
